f46000db3a22894141c52b765dae75ab9fdc2e0e
Samiul Alim <siamahmed64038@gmail.com>
2025-01-02 09:37:45 +0600

    commit

---
+++ PointOfSale/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PointOfSale.Data;
+
+namespace PointOfSale.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        private readonly PointOfSaleContext _context;
+        
+        public AdminController(PointOfSaleContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PointOfSale/Data/PointOfSaleContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+//using PointOfSale.Areas.Admin.Models;
+namespace PointOfSale.Data;
+
+public class PointOfSaleContext : IdentityDbContext<IdentityUser>
+{
+    public PointOfSaleContext(DbContextOptions<PointOfSaleContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
